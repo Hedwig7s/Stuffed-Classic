24a8a2bd302d2f372dd60500dfba3f91c3d2e8a7
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2025-01-23 21:58:59 +0100

    Replace unneeded regex

--- src/utility/sanitizenetworkstring.ts
+++ src/utility/sanitizenetworkstring.ts
@@ -1,3 +1,5 @@
 export function sanitizeNetworkString(str: string): string {
-    return str.replace(/ *$/, "");
+    let end = 1;
+    while (end <= str.length && str[end-1] !== " ") end++;
+    return str.slice(1,end);
 }
