2870ddb905279bdbaf86537554a81c038e2b1a65
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2025-01-23 22:23:15 +0100

    Dumb fuck

--- src/utility/sanitizenetworkstring.ts
+++ src/utility/sanitizenetworkstring.ts
@@ -1,5 +1,5 @@
 export function sanitizeNetworkString(str: string): string {
-    let end = 1;
-    while (end <= str.length && str[end - 1] !== " ") end++;
+    let end = str.length;
+    while (end > 0 && str[end - 1] === " ") end--;
     return str.slice(0, end);
 }
